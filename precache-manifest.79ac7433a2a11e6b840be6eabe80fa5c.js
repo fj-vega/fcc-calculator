7bf2a4fdb86f23f425611adb33aaee14fbc3479b
Stahl <fvj.114@hotmail.com>
2019-07-24 12:32:25 +0200

    Updates

---
+++ precache-manifest.79ac7433a2a11e6b840be6eabe80fa5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5677990539b015365404ca1891009855",
+    "url": "/fcc-calculator/index.html"
+  },
+  {
+    "revision": "e1a13fe3ae34bbad1179",
+    "url": "/fcc-calculator/static/css/main.900f8c68.chunk.css"
+  },
+  {
+    "revision": "2c650de91a00c587a08b",
+    "url": "/fcc-calculator/static/js/2.29acfd44.chunk.js"
+  },
+  {
+    "revision": "e1a13fe3ae34bbad1179",
+    "url": "/fcc-calculator/static/js/main.12d9daf7.chunk.js"
+  },
+  {
+    "revision": "330ba2f27051201e7f23",
+    "url": "/fcc-calculator/static/js/runtime~main.e9687150.js"
+  }
+]);
